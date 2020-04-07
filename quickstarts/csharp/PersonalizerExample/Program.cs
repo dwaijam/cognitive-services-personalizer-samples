9a5ae225b5e9ed25602827a4e0334f96ca0e5e52
Dwaipayan Mukhopadhyay <dwaijam@gmail.com>
2020-04-07 14:52:25 -0700

    add file for perftest

--- quickstarts/csharp/PersonalizerExample/Program.cs
+++ quickstarts/csharp/PersonalizerExample/Program.cs
@@ -1,43 +1,36 @@
-﻿// <Dependencies>
-using Microsoft.Azure.CognitiveServices.Personalizer;
+﻿using Microsoft.Azure.CognitiveServices.Personalizer;
 using Microsoft.Azure.CognitiveServices.Personalizer.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-// </Dependencies>
+using System.Threading.Tasks;
 
 namespace PersonalizerExample
 {
     class Program
     {
-        // <classVariables>
-        // The key specific to your personalizer resource instance; e.g. "0123456789abcdef0123456789ABCDEF"
-        private static readonly string ApiKey = Environment.GetEnvironmentVariable("PERSONALIZER_RESOURCE_KEY");
+        private const string ServiceEndpoint = "https://loop.ppe.cognitiveservices.azure.com/"; private const string ApiKey = "";
 
-        // The endpoint specific to your personalizer resource instance; e.g. https://westus2.api.cognitive.microsoft.com/
-        private static readonly string ServiceEndpoint = Environment.GetEnvironmentVariable("PERSONALIZER_RESOURCE_ENDPOINT");
-        // </classVariables>
-
-        // <mainLoop>
-        static void Main(string[] args)
+        static void Main(string[] _)
         {
-            int iteration = 1;
-            bool runLoop = true;
+            int totalCalls = 500;
+            int experimentalUnitDuration = 1;
+            int noRewardFrequency = 3; // Every nth reward = 0
+            int danglingRewardFrequency = 5; // Every nth reward = 0
+            int rewardWaitTime = (int)((experimentalUnitDuration + 1) * TimeSpan.FromMinutes(1).TotalMilliseconds);
 
             // Get the actions list to choose from personalizer with their features.
             IList<RankableAction> actions = GetActions();
 
             // Initialize Personalizer client.
             PersonalizerClient client = InitializePersonalizerClient(ServiceEndpoint);
-
+            int iteration = 0;
             do
             {
-                Console.WriteLine("\nIteration: " + iteration++);
+                Console.WriteLine("\nIteration: " + ++iteration);
 
-                // <rank>
                 // Get context information from the user.
-                string timeOfDayFeature = GetUsersTimeOfDay();
-                string tasteFeature = GetUsersTastePreference();
+                string timeOfDayFeature = GetUsersTimeOfDay(iteration);
+                string tasteFeature = GetUsersTastePreference(iteration);
 
                 // Create current context from user specified data.
                 IList<object> currentContext = new List<object>() {
@@ -46,37 +39,23 @@ namespace PersonalizerExample
                 };
 
                 // Exclude an action for personalizer ranking. This action will be held at its current position.
-                // This simulates a business rule to force the action "juice" to be ignored in the ranking.
-                // As juice is excluded, the return of the API will always be with a probability of 0.
                 IList<string> excludeActions = new List<string> { "juice" };
 
                 // Generate an ID to associate with the request.
-                string eventId = Guid.NewGuid().ToString();
+                string eventId = iteration.ToString();
 
                 // Rank the actions
                 var request = new RankRequest(actions, currentContext, excludeActions, eventId);
                 RankResponse response = client.Rank(request);
-                // </rank>
 
                 Console.WriteLine("\nPersonalizer service thinks you would like to have: " + response.RewardActionId + ". Is this correct? (y/n)");
 
-                // <reward>
                 float reward = 0.0f;
-                string answer = GetKey();
 
-                if (answer == "Y")
+                if (iteration % noRewardFrequency != 0)
                 {
                     reward = 1;
                     Console.WriteLine("\nGreat! Enjoy your food.");
-                }
-                else if (answer == "N")
-                {
-                    reward = 0;
-                    Console.WriteLine("\nYou didn't like the recommended food choice.");
-                }
-                else
-                {
-                    Console.WriteLine("\nEntered choice is invalid. Service assumes that you didn't like the recommended food choice.");
                 }
 
                 Console.WriteLine("\nPersonalizer service ranked the actions with the probabilities as below:");
@@ -86,17 +65,17 @@ namespace PersonalizerExample
                 }
 
                 // Send the reward for the action based on user response.
-                client.Reward(response.EventId, new RewardRequest(reward));
-                // </reward>
+                SendReward(response.EventId, reward, iteration % danglingRewardFrequency == 0 ? rewardWaitTime : 1000, client);
 
-                Console.WriteLine("\nPress q to break, any other key to continue:");
-                runLoop = !(GetKey() == "Q");
-
-            } while (runLoop);
+            } while (iteration < totalCalls);
         }
-        // </mainLoop>
 
-        // <authorization>
+        private static async void SendReward(string eventId, float rewardValue, int rewardWaitTimeInMillis, PersonalizerClient client)
+        {
+            await Task.Delay(rewardWaitTimeInMillis);
+            client.Reward(eventId, new RewardRequest(rewardValue));
+        }
+
         /// <summary>
         /// Initializes the personalizer client.
         /// </summary>
@@ -105,53 +84,32 @@ namespace PersonalizerExample
         static PersonalizerClient InitializePersonalizerClient(string url)
         {
             PersonalizerClient client = new PersonalizerClient(
-                new ApiKeyServiceClientCredentials(ApiKey)) { Endpoint = url };
+                new ApiKeyServiceClientCredentials(ApiKey))
+            { Endpoint = url };
 
             return client;
         }
-        // </authorization>
 
-        // <createUserFeatureTimeOfDay>
         /// <summary>
         /// Get users time of the day context.
         /// </summary>
         /// <returns>Time of day feature selected by the user.</returns>
-        static string GetUsersTimeOfDay()
+        static string GetUsersTimeOfDay(int iteration)
         {
             string[] timeOfDayFeatures = new string[] { "morning", "afternoon", "evening", "night" };
-
-            Console.WriteLine("\nWhat time of day is it (enter number)? 1. morning 2. afternoon 3. evening 4. night");
-            if (!int.TryParse(GetKey(), out int timeIndex) || timeIndex < 1 || timeIndex > timeOfDayFeatures.Length)
-            {
-                Console.WriteLine("\nEntered value is invalid. Setting feature value to " + timeOfDayFeatures[0] + ".");
-                timeIndex = 1;
-            }
-
-            return timeOfDayFeatures[timeIndex - 1];
+            return timeOfDayFeatures[iteration % timeOfDayFeatures.Length];
         }
-        // </createUserFeatureTimeOfDay>
 
-        // <createUserFeatureTastePreference>
         /// <summary>
         /// Gets user food preference.
         /// </summary>
         /// <returns>Food taste feature selected by the user.</returns>
-        static string GetUsersTastePreference()
+        static string GetUsersTastePreference(int iteration)
         {
             string[] tasteFeatures = new string[] { "salty", "sweet" };
-
-            Console.WriteLine("\nWhat type of food would you prefer (enter number)? 1. salty 2. sweet");
-            if (!int.TryParse(GetKey(), out int tasteIndex) || tasteIndex < 1 || tasteIndex > tasteFeatures.Length)
-            {
-                Console.WriteLine("\nEntered value is invalid. Setting feature value to " + tasteFeatures[0] + ".");
-                tasteIndex = 1;
-            }
-
-            return tasteFeatures[tasteIndex - 1];
+            return tasteFeatures[iteration % tasteFeatures.Length];
         }
-        // </createUserFeatureTastePreference>
 
-        // <createAction>
         /// <summary>
         /// Creates personalizer actions feature list.
         /// </summary>
@@ -191,13 +149,5 @@ namespace PersonalizerExample
 
             return actions;
         }
-        // </createAction>
-
-        // <readCommandLine>
-        private static string GetKey()
-        {
-            return Console.ReadKey().Key.ToString().Last().ToString().ToUpper();
-        }
-        // </readCommandLine>
     }
 }
